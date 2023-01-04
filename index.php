6be605e3bb80c4748bc6985b609c00e6fedb04c7
djbehar <djbehar93@gmail.com>
2023-01-04 16:05:29 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<link href="/styles.css" media="screen" rel="stylesheet" type="text/css" />
+<link href="/nav.css" media="screen" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body>
+
+<?php include('header.html');?>
+<?php include('navigation.html');?>
+
+
+<br><br><br>
+
+
+<div class="contentWrapper" style="width:100%;">
+
+
+
+
+<img src="/media/23rdSt.jpg" style="max-width:50%;min-width:250px;margin:0 auto;">
+
+
+
+
+<br>
+       <div style="
+       width:50%;
+       font-size:10px;
+       font-size:calc(5px + .5vw);
+       text-align:right;
+       margin: 0 auto;
+       min-width: 250px;
+       ">
+       <a href="https://vimeo.com/andyrobinson1">Andy Robinson</a> took this photo.
+     </div>
+       <br><br>
+
+
+</div>
+
+
+
+<br>
+
+
+
+</body>
+</html>
